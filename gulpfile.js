2fb7029604c512d232785923e58f77c4ac939099
Willi Krappen <stackola@arch-tower.lan>
2018-01-18 00:59:59 +0100

    add live browser reload

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,14 @@
 var gulp = require('gulp');
 var less = require('gulp-less'); 
+var webpack = require('webpack-stream');
 var browserSync = require('browser-sync').create();
 var reload      = browserSync.reload;
  
- 
+ var path = require('path');
+
+var BUILD_DIR = path.resolve(__dirname, './bundle/');
+var APP_DIR = path.resolve(__dirname, './');
+
 /* Task to compile less */
 gulp.task('compile-less', function() {  
   gulp.src('./less/main.less')
@@ -14,7 +19,27 @@ gulp.task('compile-less', function() {
 gulp.task('watch-less', function() {  
   gulp.watch('./less/**/*.less' , ['compile-less']);
 });
- 
+gulp.task('webpack', function() {
+  return gulp.src('./index.js')
+  .pipe(webpack({
+    watch: true,
+    entry: ["whatwg-fetch", "babel-polyfill", "./index.js"],
+  output: {
+    path: BUILD_DIR,
+    filename: 'bundle.js'
+  },
+  module : {
+    loaders : [
+      {
+        test : /\.js?/,
+        include : APP_DIR,
+        loader : 'babel-loader'
+      }
+    ]
+  }
+  }))
+  .pipe(gulp.dest('bundle/'));
+});
 gulp.task('serve', function () {
  
     // Serve files from the root of this project
@@ -25,7 +50,8 @@ gulp.task('serve', function () {
     }); 
     gulp.watch("./less/*.less").on("change", reload);
     gulp.watch("./*.html").on("change", reload);
+    gulp.watch("./bundle/*.js").on("change", reload);
 });
  
 /* Task when running `gulp` from terminal */
-gulp.task('default', ['watch-less', 'serve']);
+gulp.task('default', ['webpack', 'watch-less', 'serve']);
